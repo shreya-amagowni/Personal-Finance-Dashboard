f196fa2783869483df30de9f5dae41fc5a8b253b
shreya-amagowni <samagowni@albany.edu>
2025-12-14 02:16:15 +0000

    working frontend + backend, added .gitignore and devcontainer

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,12 @@ import { connect } from 'mongoose';
 import cors from 'cors';
 import transactionsRoutes from './routes/transactions.js';
 import ratesRoutes from './routes/rates.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors());
@@ -21,8 +25,17 @@ connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
+// Serve static files from the frontend build
+app.use(express.static(path.join(__dirname, '..','frontend', 'dist')));
+
+// For any route not handled by API, serve index.html
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
+
 // --- Start the Server --- //
-app.listen(5000, () => {
-  console.log("Backend Server running on http://localhost:5000");
+const PORT = 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend Server running on http://localhost:${PORT}`);
 });
 
